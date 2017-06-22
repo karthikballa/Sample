447bb28aa8f493f140bb87ca781bbf745ad1519b
karthikballa <karthik333.b@gmail.com>
2017-06-23 00:50:38 +0530

    publish

---
+++ project1/project1/Models/Mymodel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project1.Models
+{
+    public class Mymodel
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+    }
+}
